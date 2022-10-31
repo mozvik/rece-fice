c1120636bd485dd8541e29889c903aec2bee0e2e
mozvik <mozvik@yahoo.com>
2022-10-31 19:53:15 +0100

    nutrition fix

--- src/app/pages/details/recipe-nutrition/recipe-nutrition.component.ts
+++ src/app/pages/details/recipe-nutrition/recipe-nutrition.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
 import { Recipe } from 'src/app/classes/recipe';
 
 @Component({
@@ -12,4 +13,12 @@ export class RecipeNutritionComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  elementQuantity(element: any): string {
+    if (element.name == 'Kalória') {
+      return element.quantity + ' kcal';
+    } else {
+      return element.quantity + ' gramm';
+    }
+  }
 }
